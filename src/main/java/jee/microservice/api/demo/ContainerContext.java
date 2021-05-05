e9577ed141a4eba4b77e14269b84dd27cf30f3a4
psujeethkumar <psujeethkumar@gmail.com>
2021-05-05 09:08:30 +0200

    Added container id retreival information

--- src/main/java/jee/microservice/api/demo/ContainerContext.java
+++ src/main/java/jee/microservice/api/demo/ContainerContext.java
@@ -5,20 +5,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.ListContainersCmd;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.core.DockerClientBuilder;
+
 @SpringBootApplication
 @RestController
 public class ContainerContext {
 
 	@GetMapping("/containerId")
 	public String returnContainerId() {
-		return "Welcome to java techie";
+		String containerId = "empty";
+		DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+		ListContainersCmd listContainersCmd = dockerClient.listContainersCmd().withShowAll(true);
+		for (Container container : listContainersCmd.exec()) {
+			containerId = container.getId();
+		}
+		return "Welcome to java techie " + containerId;
 	}
 
 	@GetMapping("/greet")
 	public String greetConsumer() {
-		return "Welcome ! I'm a microservice running in beaturiful world of Openshift platform";
+		return "Welcome ! I'm a microservice running in beatutiful world of Openshift platform";
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ContainerContext.class, args);
 	}
