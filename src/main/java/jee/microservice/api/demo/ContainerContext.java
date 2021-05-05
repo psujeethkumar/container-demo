f350c141197ab92cd3ee9b6dc920c257158ea9fb
psujeethkumar <psujeethkumar@gmail.com>
2021-05-05 09:54:40 +0200

    Updated

--- src/main/java/jee/microservice/api/demo/ContainerContext.java
+++ src/main/java/jee/microservice/api/demo/ContainerContext.java
@@ -1,14 +1,12 @@
 package jee.microservice.api.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.ListContainersCmd;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.core.DockerClientBuilder;
 
 @SpringBootApplication
 @RestController
@@ -17,11 +15,12 @@ public class ContainerContext {
 	@GetMapping("/containerId")
 	public String returnContainerId() {
 		String containerId = "empty";
-		DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-		ListContainersCmd listContainersCmd = dockerClient.listContainersCmd().withShowAll(true);
-		for (Container container : listContainersCmd.exec()) {
-			containerId = container.getId();
-		}		
+		try {
+			InetAddress id = InetAddress.getLocalHost();
+			containerId = id.getHostName();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		return "Welcome to java techie " + containerId;
 	}
 
