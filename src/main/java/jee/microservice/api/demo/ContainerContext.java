3f1e771f7f89918d3395a2b0a0950054df75b144
psujeethkumar <psujeethkumar@gmail.com>
2021-05-05 08:46:39 +0200

    Initial commit

---
+++ src/main/java/jee/microservice/api/demo/ContainerContext.java
@@ -0,0 +1,26 @@
+package jee.microservice.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ContainerContext {
+
+	@GetMapping("/containerId")
+	public String returnContainerId() {
+		return "Welcome to java techie";
+	}
+
+	@GetMapping("/greet")
+	public String greetConsumer() {
+		return "Welcome ! I'm a microservice running in beaturiful world of Openshift platform";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerContext.class, args);
+	}
+
+}
